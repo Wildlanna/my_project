3427504f6da58fa82ee2f0b28115ccbd9de7418f
Mims <miriama.romanov@gmail.com>
2024-07-18 11:26:06 +0200

    counter

--- skicarik.py
+++ skicarik.py
@@ -1,4 +1,13 @@
 
 
 x=[3,1.3,True,'alfa']
-print(x)
+print(type(x))
+
+<pyscript>
+function runPythonScript() {
+    # Get the path to the Python script.
+    var pythonScriptPath = "./skicarik.py";
+    # Run the Python script.
+    subprocess.run(["python", pythonScriptPath]);
+    }
+</pyscript>
